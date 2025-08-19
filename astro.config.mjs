5001365ddabdc13c3afe85c7ee33ef2d827f27de
L15451 <alvarezjoaquine@gmail.com>
2025-08-19 17:49:27 +0000

    fix: update asset and route paths to use BASE_URL for deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    site: 'https://escobar.gob.ar',
+    base: '/prueba-barrios',
+    build: {
+        assets: '_astro',
+        inlineStylesheets: 'auto'
+    },
+    vite: {
+        build: {
+            minify: true,
+            cssMinify: true,
+            cssCodeSplit: true,
+        },
+    },
+});
